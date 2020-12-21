82fc1c97bf86ae69d10cc4b373b798ed235b8072
Ezra Tassone <etassone@gmail.com>
2020-12-21 09:06:39 +0800

    Displaying bar and bubble charts

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,6 @@
-
+// Function to generate HTML code for display of demographic information of a subject in panel
 function displayDemoInfo (subjectID) {
+
     // Name and path to JSON file with dataset 
     let json_file = "samples.json";
 
@@ -31,6 +32,88 @@ function displayDemoInfo (subjectID) {
     });
 }
 
+// Function to plot bar and bubble charts
+function displayPlots (subjectID) {
+
+    // Name and path to JSON file with dataset 
+    let json_file = "samples.json";
+
+    // Read in JSON file with D3
+    d3.json(json_file).then(function(data) {
+
+        // Filter metadata set to return only sample data for matching ID and subjectID function parameter
+        // i.e. Only returns metadata for the desired subject
+        let subject_samples = data.samples.filter(subject => subject.id == subjectID)
+
+        // Display metadata to console for checking
+        // Using an index of 0 as subject_samples is an array of length 1
+        console.log(subject_samples[0]);
+
+        // Retrieve Top 10 sample values 
+        // Already sorted in JSON data so can use .slice for values
+        let sampleValuesTop10 = subject_samples[0].sample_values.slice(0,10);
+
+        // Display sample values to console for checking
+        console.log(sampleValuesTop10);
+
+        // Retrieve otu IDs for Top 10 sample values
+        let otuIDsTop10 = subject_samples[0].otu_ids.slice(0,10);
+
+        // Display otu IDs to console for checking
+        console.log(otuIDsTop10);
+
+        // Convert otu IDs to array of strings with value "OTU {otuID}"
+        // To provide clearer display in bar chart
+        let otuIDsList = otuIDsTop10.map(otuID => `OTU ${otuID}`)
+
+        // Display strings to console for checking
+        console.log(otuIDsList);
+
+        // Retrieve otu labels for use in hovertext of bar chart
+        let otuLabelsTop10 = subject_samples[0].otu_labels.slice(0,10);
+
+        // Display labels to console for checking
+        console.log(otuLabelsTop10);
+
+        // Set up data for horizontal bar plot
+        // Must use reverse on arrays to display in descending order of sample values
+        let barData = [{
+            type: "bar",
+            x: sampleValuesTop10.reverse(),
+            y: otuIDsList.reverse(),
+            type: "bar",
+            orientation: "h",
+            text: otuLabelsTop10.reverse()
+          }];
+          
+        // Use plotly to display bar chart at div ID "bar" with barData
+        Plotly.newPlot('bar', barData);
+
+        // Set up data for bubble chart
+        // Parameters set as given in assignment instructions
+        let  bubbleData = [{
+            x: subject_samples[0].otu_ids,
+            y: subject_samples[0].sample_values,
+            mode: "markers",
+            marker: {
+              color: subject_samples[0].otu_ids,
+              size:subject_samples[0].sample_values,
+            },
+            text: subject_samples[0].otu_labels
+          }];
+
+        // Set name of x-axis for bubble chart
+        let bubbleLayout = {
+            xaxis: { title: "OTU ID" }
+        };
+
+        // Use plotly to display bubble chart at div ID "bubble" with barData and barLayout
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+    });
+}
+
+
 // Call function to update displays of demographic information panel and plots
 // when the dropdown menu selection is changed
 d3.selectAll("#selDataset").on("change", optionChanged);
@@ -46,11 +129,13 @@ function optionChanged() {
     
     // Display the demographic information panel for the desired subject
     displayDemoInfo(subject);
+    displayPlots(subject);
 
 }
 
 // Initialisation function
 function init() {
+
     // Name and path to JSON file with dataset 
     let json_file = "samples.json";
 
@@ -69,8 +154,9 @@ function init() {
             dropdownMenu.append("option").attr("value", nameID).text(nameID);
         });
         
+    // Initialise page with demographic information and plots for first subject in dataset    
     displayDemoInfo(data.names[0]);
-
+    displayPlots(data.names[0])
 
   });
   
